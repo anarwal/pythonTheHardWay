c8f4cf2dd301bbac41a3eb99c9b5fd8f7afa32b3
Abhimanyu Narwal <abhimanyunarwal@northwesternmutual.com>
2020-07-06 07:32:24 -0400

    first program

---
+++ calculator.py
@@ -0,0 +1,27 @@
+#addition
+print("Addition")
+num1=input("Add first numbers you want to add: ")
+num2=input("Add second numbers you want to add: ")
+result = float(num1)+float(num2)
+print(result)
+
+#subtraction
+print("Subtraction")
+num1=input("Add first numbers you want to subtract: ")
+num2=input("Add second numbers you want to subtract: ")
+result = float(num1)-float(num2)
+print(result)
+
+#division
+print("Division")
+num1=input("Add first numbers you want to divide: ")
+num2=input("Add second numbers you want to divide: ")
+result = float(num1)/float(num2)
+print(result)
+
+#multiplication
+print("Multiplication")
+num1=input("Add first numbers you want to multiply: ")
+num2=input("Add second numbers you want to multiply: ")
+result = float(num1)*float(num2)
+print(result)
