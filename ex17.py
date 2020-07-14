a818b1f4077727538575b3c6eb297149ee8b32f5
Abhimanyu Narwal <anarwal.27@gmail.com>
2020-07-14 08:56:25 -0400

    copying data between files

---
+++ ex17.py
@@ -0,0 +1,18 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f'Copying data from {from_file} to {to_file}')
+
+in_file_data = open(from_file).read()
+
+print(f'Input file is len{from_file} bytes')
+
+print(f'Does the output file exists? {exists(to_file)}')
+print('Hit Return if you wish to continue, else hit Ctrl-C')
+
+
+out_file_data = open(to_file, 'w').write(in_file_data)
+
+print('Copying done')
